7fad1f709d1109ad5d4b80d6b41440b8b82380da
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-12 06:43:37 +0530

    Update terraform/

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -1,4 +1,5 @@
 variable "demo" {
+  type    = string
   default = "Hello World"
 }
 
@@ -6,4 +7,27 @@ output "demo" {
   value = var.demo
 }
 
-variable "demo1" {}
+variable "demo1" {
+  default = 100
+  type    = number
+}
+
+variable "demo2" {
+  description = "Demo variable for bool demo"
+  default     = true
+  type        = bool
+}
+
+variable "demo3" {
+  description = "List Variable"
+  default     = [100, "abc"]
+}
+
+variable "demo4" {
+  description = "Map Variable"
+  default = {
+    course     = "DevOps"
+    timing     = "6am"
+    is_started = true
+  }
+}
