02380ecad33ca622032ab4f9600f125061219b67
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-20 06:34:36 +0530

    Update terraform/

--- 07-conditions/sample.tf
+++ 07-conditions/sample.tf
@@ -11,3 +11,11 @@ variable "a" {
 resource "null_resource" "sample" {
   count = var.a == 1 ? 1 : 0
 }
+
+variable "b" {
+  default = false
+}
+
+resource "null_resource" "sample1" {
+  count = var.b ? 1 : 0
+}
